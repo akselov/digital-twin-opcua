9846b039cc81620c393f5fc31844c0981839886d
akselov <32615693+akselov@users.noreply.github.com>
2018-05-10 10:15:53 +0200

    FreeOpcUa Python Library

---
+++ python-opcua-master/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+import sys
+
+install_requires = ["python-dateutil", "pytz", "lxml"]
+if sys.version_info[0] < 3:
+    install_requires.extend(["enum34", "trollius", "futures"])
+
+setup(name="opcua",
+      version="0.95.2",
+      description="Pure Python OPC-UA client and server library",
+      author="Olivier Roulet-Dubonnet",
+      author_email="olivier.roulet@gmail.com",
+      url='http://freeopcua.github.io/',
+      packages=find_packages(),
+      provides=["opcua"],
+      license="GNU Lesser General Public License v3 or later",
+      install_requires=install_requires,
+      extras_require={
+          'encryption': ['cryptography']
+      },
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 2",
+                   "Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "Operating System :: OS Independent",
+                   "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      entry_points={'console_scripts':
+                    [
+                        'uaread = opcua.tools:uaread',
+                        'uals = opcua.tools:uals',
+                        'uabrowse = opcua.tools:uals',
+                        'uawrite = opcua.tools:uawrite',
+                        'uasubscribe = opcua.tools:uasubscribe',
+                        'uahistoryread = opcua.tools:uahistoryread',
+                        'uaclient = opcua.tools:uaclient',
+                        'uaserver = opcua.tools:uaserver',
+                        'uadiscover = opcua.tools:uadiscover',
+                        'uacall = opcua.tools:uacall',
+                    ]
+                    }
+      )
+
